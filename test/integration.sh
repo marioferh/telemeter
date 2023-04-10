dd5994c030bc02de70386643f6d6abdc6ef1676c
Mario Fernandez <mariofer@redhat.com>
2023-04-10 11:55:23 +0200

    Change integration paths, refactor
    Signed-off-by: Mario Fernandez <mariofer@redhat.com>

--- test/integration.sh
+++ test/integration.sh
@@ -2,9 +2,7 @@
 
 # Runs a semi-realistic integration test with two servers, a stub authorization server, a 
 # prometheus that scrapes from them, and a single client that fetches "cluster" metrics.
-# If no arguments are passed an integration test scenario is run. Otherwise $1 becomes 
-# the upstream prometheus server to test against and $2 is an optional bearer token to 
-# authenticate the request.
+# If no arguments are passed an integration test scenario is run.
 
 set -euo pipefail
 
@@ -13,7 +11,7 @@ trap 'kill $(jobs -p); exit $result' EXIT
 
 ( ./authorization-server localhost:9001 ./test/tokens.json ) &
 
-( prometheus --config.file=./test/prom-local.conf --web.listen-address=localhost:9090 "--storage.tsdb.path=$(mktemp -d)" --log.level=warn ) &
+( ./_output/bin/prometheus --config.file=./test/prom-local.conf --web.listen-address=localhost:9090 "--storage.tsdb.path=$(mktemp -d)" --log.level=warn ) &
 
 ( 
   sleep 5
@@ -43,46 +41,48 @@ trap 'kill $(jobs -p); exit $result' EXIT
 ) &
 
 (
-thanos receive \
+./_output/bin/thanos receive \
     --tsdb.path="$(mktemp -d)" \
     --remote-write.address=127.0.0.1:9005 \
     --grpc-address=127.0.0.1:9006
 ) &
 
 (
-thanos query \
+./_output/bin/thanos query \
     --grpc-address=127.0.0.1:9007 \
     --http-address=127.0.0.1:9008 \
     --store=127.0.0.1:9006
 ) &
 
-sleep 1
+#Wait for all components correct state
+sleep 30
 
 retries=100
 while true; do
+ echo "check succesfuly retrieve metrics from local prometheus server"
   if [[ "${retries}" -lt 0 ]]; then
     echo "error: Did not successfully retrieve cluster metrics from the local Prometheus server" 1>&2
     exit 1
   fi
-  # verify we scrape metrics from the test cluster and give it _id test
+  echo "verify we scrape metrics from the test cluster and give it _id test"
   if [[ "$( curl http://localhost:9008/api/v1/query --data-urlencode 'query=count({_id="test"})' -G 2>/dev/null | python3 -c 'import sys, json; print(json.load(sys.stdin)["data"]["result"][0]["value"][1])' 2>/dev/null )" -eq 0 ]]; then
     retries=$((retries-1))
     sleep 1
     continue
   fi
-  # verify we rename scrape_samples_scraped to scraped
+  echo "verify we rename scrape_samples_scraped to scraped"
   if [[ "$( curl http://localhost:9008/api/v1/query --data-urlencode 'query=count(scraped{_id="test"})' -G 2>/dev/null | python3 -c 'import sys, json; print(json.load(sys.stdin)["data"]["result"][0]["value"][1])' 2>/dev/null )" -eq 0 ]]; then
     retries=$((retries-1))
     sleep 1
     continue
   fi
-  # verify we got alerts as remapped from ALERTS
+  echo "verify we got alerts as remapped from ALERTS"
   if [[ "$( curl http://localhost:9008/api/v1/query --data-urlencode 'query=count(alerts{_id="test"})' -G 2>/dev/null | python3 -c 'import sys, json; print(json.load(sys.stdin)["data"]["result"][0]["value"][1])' 2>/dev/null )" -eq 0 ]]; then
     retries=$((retries-1))
     sleep 1
     continue
   fi
-  # verify we don't get elided labels
+  echo "verify we don't get elided labels"
   if [[ "$( curl http://localhost:9008/api/v1/query --data-urlencode 'query=count(alerts{_id="test",_elide=~".+"})' -G 2>/dev/null | python3 -c 'import sys, json; print(len(json.load(sys.stdin)["data"]["result"]))' 2>/dev/null )" -gt 0 ]]; then
     retries=$((retries-1))
     sleep 1
@@ -90,7 +90,8 @@ while true; do
   fi
   break
 done
-echo "tests: ok"
+echo "\n ALL TEST PASSED!!" 
+sleep 5
 result=0
 exit 0
 
